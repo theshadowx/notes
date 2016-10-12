a4ace8a003af3356d23d80ff427524816eed2ed5
Ali Diouri <alidiouri@gmail.com>
2016-10-12 22:21:42 +0100

    adding context menu to the item to add a subfolder or to delete the folder

--- src/folderwidgetdelegate.cpp
+++ src/folderwidgetdelegate.cpp
@@ -1,10 +1,12 @@
 #include "folderwidgetdelegate.h"
 #include <QKeyEvent>
 #include <QLineEdit>
-#include <QDebug>
 #include <QApplication>
+#include <QMouseEvent>
+#include <QMenu>
+#include <QAction>
 
-FolderWidgetDelegate::FolderWidgetDelegate(QObject*parent)
+FolderWidgetDelegate::FolderWidgetDelegate(QObject* parent)
     : QStyledItemDelegate(parent)
 {
 
@@ -18,8 +20,55 @@ QWidget* FolderWidgetDelegate::createEditor(QWidget *parent,
 
     QLineEdit* e = qobject_cast<QLineEdit*>(editor);
     QRegExp rx("[a-zA-Z0-9]{1,12}");
-    QValidator *validator = new QRegExpValidator(rx, (QObject*)this);
+    QValidator *validator = new QRegExpValidator(rx, editor);
     e->setValidator(validator);
 
     return e;
+}
+
+bool FolderWidgetDelegate::editorEvent(QEvent* event,
+                                       QAbstractItemModel* model,
+                                       const QStyleOptionViewItem& option,
+                                       const QModelIndex& index)
+{
+
+    switch (event->type()) {
+    case QEvent::MouseButtonRelease:{
+        QMouseEvent* mouseEvent = static_cast<QMouseEvent*>(event);
+        if(mouseEvent->button() == Qt::RightButton){
+            showContextMenu(model, option, index);
+        }
+        break;
+    }
+    default:
+        break;
+    }
+
+    return QStyledItemDelegate::editorEvent(event, model, option, index);
+}
+
+void FolderWidgetDelegate::showContextMenu(QAbstractItemModel* model,
+                                           const QStyleOptionViewItem& option,
+                                           const QModelIndex& index)
+{
+    Q_UNUSED(model)
+    Q_UNUSED(option)
+
+    QMenu optionMenu;
+
+    QAction addSubFolder(tr("Add Subfolder"),&optionMenu);
+    addSubFolder.setIcon(QIcon(QStringLiteral(":/images/newNote_Regular.png")));
+
+    QAction deleteSubFolder(tr("Delete folder"),&optionMenu);
+    deleteSubFolder.setIcon(QIcon(QStringLiteral(":/images/trashCan_Regular.png")));
+
+    optionMenu.addAction(&addSubFolder);
+    optionMenu.addAction(&deleteSubFolder);
+    QAction* selectedAction = optionMenu.exec(QCursor::pos());
+
+    if(selectedAction == &addSubFolder){
+        emit addSubFolderClicked(index);
+    }else if(selectedAction == &deleteSubFolder){
+        emit deleteSubFolderButtonClicked(index);
+    }
 }
--- src/folderwidgetdelegate.h
+++ src/folderwidgetdelegate.h
@@ -2,15 +2,32 @@
 #define FOLDERWIDGETDELEGATE_H
 
 #include <QStyledItemDelegate>
+#include <QPushButton>
+#include <QTreeView>
 
 class FolderWidgetDelegate : public QStyledItemDelegate
 {
+    Q_OBJECT
+
 public:
-    explicit FolderWidgetDelegate(QObject* parent = 0);
+    explicit FolderWidgetDelegate(QObject* parent = Q_NULLPTR);
 
     QWidget* createEditor(QWidget *parent,
                           const QStyleOptionViewItem &option,
                           const QModelIndex &index) const Q_DECL_OVERRIDE;
+
+    bool editorEvent(QEvent *event,
+                     QAbstractItemModel *model,
+                     const QStyleOptionViewItem &option,
+                     const QModelIndex &index) Q_DECL_OVERRIDE;
+
+    void showContextMenu(QAbstractItemModel*model,
+                         const QStyleOptionViewItem&option,
+                         const QModelIndex&index);
+
+signals:
+    void addSubFolderClicked(QModelIndex index);
+    void deleteSubFolderButtonClicked(QModelIndex index);
 };
 
 #endif // FOLDERWIDGETDELEGATE_H

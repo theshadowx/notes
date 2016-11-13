de3107893f37a7debdbe06c7e1dfea81e8513d24
Ali Diouri <alidiouri@gmail.com>
2016-11-13 18:36:30 +0000

    Adding function to map the input index to the source model
    Emitting layout changed signal when the source model is set.

--- src/tagnotemodel.h
+++ src/tagnotemodel.h
@@ -11,6 +11,8 @@ class TagNoteModel: public QAbstractListModel
 public:
     explicit TagNoteModel(QObject* parent = Q_NULLPTR);
 
+    QModelIndex mapToSource(QModelIndex index);
+
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
     bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) Q_DECL_OVERRIDE;
     Qt::ItemFlags flags(const QModelIndex &index) const Q_DECL_OVERRIDE;
